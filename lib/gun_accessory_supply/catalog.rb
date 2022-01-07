5133e5572a45bbfff7a450edd5f4d47f3cfd5de3
Tony Beninate <tonybeninate@icloud.com>
2022-01-07 10:58:30 -0500

    Normalize category data to Category/Subcategory. #1

--- lib/gun_accessory_supply/catalog.rb
+++ lib/gun_accessory_supply/catalog.rb
@@ -23,16 +23,24 @@ module GunAccessorySupply
           next
         end
 
+        category = row[@headers.index('Sub-Category')].try(:strip)
+
         item = {
           mfg_number:      row[@headers.index('Item ID')].try(:strip),
           upc:             row[@headers.index('UPC')].try(:strip),
           name:            row[@headers.index('Item Description')].try(:strip),
           quantity:        row[@headers.index("Available Qty")].to_i,
-          price:           row[@headers.index('MSRP')].try(:strip), # FIXME: Ensure this is the correct value
+          price:           row[@headers.index('MSRP')].try(:strip),
+          msrp:            row[@headers.index('MSRP')].try(:strip),
+          map_price:       row[@headers.index('MAP Price')].try(:strip),
           brand:           row[@headers.index('Manufacturer')].try(:strip),
           item_identifier: row[@headers.index("Image Link")].try(:strip),
-          category:        row[@headers.index('Category')].try(:strip),
-          subcategory:     row[@headers.index('Category')].try(:strip), # FIXME: Need a sub-cat
+          category:        cleaned_category(category)[0],
+          subcategory:     cleaned_category(category)[1],
+          weight:          row[@headers.index('Shipping Weight')].try(:strip),
+          features: {
+            caliber:       row[@headers.index('Caliber')].try(:strip),
+          }
         }
 
         items << item
@@ -42,6 +50,39 @@ module GunAccessorySupply
       tempfile.unlink
 
       items
+    end
+
+    private
+
+    # Returns an array of ["Category", "Subcategory"]
+    def cleaned_category(category_string)
+      # ex. category_string = "Gun Care/Security/Safes"
+      categories = category_string.split('/')
+
+      category = case categories.first
+      when 'Gun Care'
+        if category_string.include?('Gun Care/Security')
+          ["Accessories", "Security"]
+        else
+          ["Accessories", "Cleaning"]
+        end
+      when 'Hunting'
+        if categories[1] == 'Knives'
+          categories[2]
+        else
+          ["Hunting", "Accessories"]
+        end
+      when 'Miscellaneous'
+        ["Accessories", "Miscellaneous"]
+      when 'Outdoor'
+        categories.last(2)
+      when 'Parts'
+        ["Accessories", "Gun Parts"]
+      when 'Shooting'
+        categories.last(2)
+      end
+
+      category.present? ? category : categories.first(2)
     end
 
   end
